352accff1420831c68e1ed8c7c1bcadf004706ef
QWC\quinw <quinwacca@gmail.com>
2025-03-21 21:08:04 +0100

    add layout container and header components, add theme, modify globals css

---
+++ src/components/layout/container/index.js
@@ -0,0 +1,9 @@
+import styles from './index.module.css'
+
+const LayoutContainer = ({children}) => {
+  return <main className={styles.main}>{children}</main>
+}
+
+LayoutContainer.displayName = 'LayoutContainer'
+
+export default LayoutContainer
