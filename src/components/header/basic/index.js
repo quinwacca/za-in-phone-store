03cc350e04eafb7d5e3c923f6e8cf30c004de71b
QWC\quinw <quinwacca@gmail.com>
2025-03-24 20:48:26 +0100

    add link to header logo

--- src/components/header/basic/index.js
+++ src/components/header/basic/index.js
@@ -1,10 +1,14 @@
+import Link from 'next/link'
+
 import Image from 'next/image'
 import styles from './index.module.css'
 
 const HeaderBasic = () => {
   return (
     <header className={styles.header}>
-      <Image className={styles.logo} src="/mbst-logo.svg" alt="MBST logo" width={74} height={29} priority />
+      <Link className={styles.logo} href="/">
+        <Image src="/mbst-logo.svg" alt="MBST logo" width={74} height={29} priority />
+      </Link>
     </header>
   )
 }
