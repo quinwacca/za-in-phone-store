66e7c86270f5bf800aeabb939497c175e48d3c07
QWC\quinw <quinwacca@gmail.com>
2025-03-24 19:03:42 +0100

    add vitest, msw, and husky

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,33 @@
+import {domain} from '@/domain'
+
 import InputSearch from '@/components/input/search'
 import styles from './page.module.css'
 
-export default function Home() {
+const HomePage = async ({searchParams}) => {
+  const searchValue = (await searchParams).search
+  const [searchProductsError, searchProductsResponse] = await domain.searchProductsUseCase({search: searchValue})
+
+  // had to remove duplicates to avoid React warning about keys
+  const products = Array.from(new Set(searchProductsResponse.map(product => product.id))).map(id =>
+    searchProductsResponse.find(product => product.id === id)
+  ) // XMI-RN13P5G appears more than once in the list
+
   return (
     <div className={styles.page}>
       <InputSearch />
       <div>
         <h2>Phones List</h2>
+        {searchProductsError && <p>{`Oops! Something went wrong!`}</p>}
+        <ul>
+          {products.map(product => {
+            return <li key={product.id}>{`${product.brand} - ${product.name}`}</li>
+          })}
+        </ul>
       </div>
     </div>
   )
 }
+
+HomePage.displayName = 'HomePage'
+
+export default HomePage
