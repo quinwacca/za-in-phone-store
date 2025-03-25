5dcc66e9d7b11dfa00437844de2a012b74a834b4
QWC\quinw <quinwacca@gmail.com>
2025-03-25 14:35:44 +0100

    move some style from layout to page

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,5 +19,5 @@ export default RootLayout
 
 export const metadata = {
   title: 'MBST - A Zara Challenge by Luis Garrido',
-  description: 'MBST - A Zara Challenge by Luis Garrido built with Next.js'
+  description: 'MBST - A Zara Challenge by Luis Garrido, built with Next.js'
 }
