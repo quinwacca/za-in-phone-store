7eb50c3d4c151097f188ad2b564745ceb1568688
QWC\quinw <quinwacca@gmail.com>
2025-03-25 10:39:46 +0100

    add fetcher layer and apply on products repository

--- src/domain/products/repositories/httpProductsRepository.js
+++ src/domain/products/repositories/httpProductsRepository.js
@@ -1,22 +1,24 @@
+import {ApiFetcher} from '@/domain/fetcher/ApiFetcher'
+
 export class HttpProductsRepository {
+  #fetcher
+  #productsApiUrl
+
   static create() {
     return new HttpProductsRepository()
   }
 
-  async searchProducts({query, limit, offset} = {}) {
-    const api = `https://prueba-tecnica-api-tienda-moviles.onrender.com/products`
+  constructor() {
+    this.#fetcher = ApiFetcher.create()
+    this.#productsApiUrl = '/products'
+  }
 
+  async searchProducts({query, limit, offset} = {}) {
     const urlSearchParams = new URLSearchParams(`?limit=${limit}&offset=${offset}`)
     if (query !== undefined) urlSearchParams.append('search', query)
 
-    const apiHeaders = new Headers()
-    // this KEY it should be stored in a .env file
-    // but we should not upload .env files to the repository
-    // so this is only for the challenge, to run easily on your local machine
-    apiHeaders.append('x-api-key', process.env.NEXT_PUBLIC_MBST_API_KEY ?? '87909682e6cd74208f41a6ef39fe4191')
+    const apiUrl = `${this.#productsApiUrl}?${urlSearchParams.toString()}`
 
-    const apiUrl = `${api}?${urlSearchParams.toString()}`
-
-    return fetch(apiUrl, {headers: apiHeaders}).then(res => res.json())
+    return this.#fetcher.get(apiUrl).then(res => res.json())
   }
 }
