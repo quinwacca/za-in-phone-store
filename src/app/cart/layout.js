9d72a0ea8a6d6569fd4c7097b33c7d3026c28cd9
QWC\quinw <quinwacca@gmail.com>
2025-03-26 18:19:08 +0100

    add cart page metadata

---
+++ src/app/cart/layout.js
@@ -0,0 +1,12 @@
+const CartPageLayout = ({children}) => {
+  return children
+}
+
+export default CartPageLayout
+
+export const generateMetadata = async () => {
+  return {
+    title: `Cart - MBST - A Zara Challenge by Luis Garrido`,
+    description: `Cart - MBST - A Zara Challenge by Luis Garrido, built with Next.js`
+  }
+}
