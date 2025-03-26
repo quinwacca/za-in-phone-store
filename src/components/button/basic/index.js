d5e4796fefaaacbca5104e591440d0c93c111f4e
QWC\quinw <quinwacca@gmail.com>
2025-03-26 18:18:43 +0100

    button basic - add outline design variant, and link type variant

--- src/components/button/basic/index.js
+++ src/components/button/basic/index.js
@@ -1,13 +1,48 @@
+import Link from 'next/link'
+
 import styles from './index.module.css'
 
-const ButtonBasic = ({children, disabled, onClick = () => {}}) => {
-  return (
-    <button className={styles.button} disabled={disabled} onClick={onClick}>
-      {children}
-    </button>
-  )
+const DESIGN_TYPES = {
+  OUTLINE: 'OUTLINE',
+  SOLID: 'SOLID'
+}
+
+const BUTTON_TYPES = {
+  LINK: 'LINK',
+  BUTTON: 'BUTTON'
+}
+
+const DESIGNS = {
+  OUTLINE: styles.buttonOutline,
+  SOLID: styles.buttonSolid
+}
+
+const ButtonBasic = ({
+  children,
+  design = DESIGN_TYPES.SOLID,
+  disabled = false,
+  onClick = () => {},
+  type = BUTTON_TYPES.BUTTON
+}) => {
+  if (type === BUTTON_TYPES.LINK) {
+    return (
+      <Link href="/" className={DESIGNS[design]} disabled={disabled}>
+        {children}
+      </Link>
+    )
+  }
+
+  if (type === BUTTON_TYPES.BUTTON) {
+    return (
+      <button className={DESIGNS[design]} disabled={disabled} onClick={onClick}>
+        {children}
+      </button>
+    )
+  }
 }
 
 ButtonBasic.displayName = 'ButtonBasic'
 
 export default ButtonBasic
+
+export {BUTTON_TYPES as buttonBasicTypes, DESIGN_TYPES as buttonBasicDesigns}
