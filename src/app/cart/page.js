68617402bf007880866a9030c37fa66ec1184a6b
QWC\quinw <quinwacca@gmail.com>
2025-03-26 16:09:54 +0100

    add simple product list on cart page, with the feature of removing products

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,7 +1,21 @@
+'use client'
+
+import {useCart} from '@/contexts/cart'
+
 const CartPage = () => {
+  const {cart, cartSize, removeFromCart} = useCart()
+
   return (
     <div>
-      <h1>Cart Page</h1>
+      <h1>Cart ({cartSize.toString()})</h1>
+      {cart.map(({id, model, selectedCapacity, selectedColor}) => (
+        <div key={`${id}-${selectedCapacity.capacity}-${selectedColor.name}`}>
+          <h2>{model}</h2>
+          <p>Color: {selectedColor.name}</p>
+          <p>Capacity: {selectedCapacity.capacity}</p>
+          <button onClick={() => removeFromCart({id, model, selectedCapacity, selectedColor})}>Remove from cart</button>
+        </div>
+      ))}
     </div>
   )
 }
