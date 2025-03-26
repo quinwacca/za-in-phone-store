1deb4be1b9598d1b0f4352034389e9b2614e214c
QWC\quinw <quinwacca@gmail.com>
2025-03-26 22:03:01 +0100

    add use cases on domain factory

--- src/domain/index.js
+++ src/domain/index.js
@@ -6,9 +6,15 @@ const importFactory = ({context, params, useCase}) =>
     .then(response => [null, response])
     .catch(error => [error, null])
 
+const cartUseCases = {
+  addToCartUseCase: params => importFactory({context: 'cart', params, useCase: 'AddToCartUseCase'}),
+  getCartUseCase: params => importFactory({context: 'cart', params, useCase: 'GetCartUseCase'}),
+  removeFromCartUseCase: params => importFactory({context: 'cart', params, useCase: 'RemoveFromCartUseCase'})
+}
+
 const productsUseCases = {
   getProductUseCase: params => importFactory({context: 'products', params, useCase: 'GetProductUseCase'}),
   searchProductsUseCase: params => importFactory({context: 'products', params, useCase: 'SearchProductsUseCase'})
 }
 
-export const domain = {...productsUseCases}
+export const domain = {...cartUseCases, ...productsUseCases}
