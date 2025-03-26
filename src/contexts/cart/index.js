e7ed84446520a0bc4e19fdbc4b2a0ef4453cb60d
QWC\quinw <quinwacca@gmail.com>
2025-03-26 15:55:37 +0100

    create cart context provider and consumer

---
+++ src/contexts/cart/index.js
@@ -0,0 +1,49 @@
+'use client'
+
+import {createContext, useContext, useState} from 'react'
+
+const CartContext = createContext()
+
+export const CartProvider = ({children}) => {
+  const [cart, setCart] = useState([])
+
+  const addToCart = newProduct => {
+    setCart(previousCart => {
+      const isProductInCart = previousCart.some(
+        ({id, selectedCapacity, selectedColor}) =>
+          id === newProduct.id &&
+          selectedCapacity.capacity === newProduct.selectedCapacity.capacity &&
+          selectedColor.name === newProduct.selectedColor.name
+      )
+
+      return isProductInCart ? [...previousCart] : [...previousCart, newProduct]
+    })
+  }
+
+  const removeFromCart = removedProduct => {
+    setCart(previousCart => {
+      const cartWithoutRemovedProduct = previousCart.filter(
+        ({id, selectedCapacity, selectedColor}) =>
+          !(
+            id === removedProduct.id &&
+            selectedCapacity.capacity === removedProduct.selectedCapacity.capacity &&
+            selectedColor.name === removedProduct.selectedColor.name
+          )
+      )
+
+      return cartWithoutRemovedProduct
+    })
+  }
+
+  return (
+    <CartContext.Provider value={{cart, cartSize: cart.length, addToCart, removeFromCart}}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export const useCart = () => {
+  const context = useContext(CartContext)
+
+  return context
+}
