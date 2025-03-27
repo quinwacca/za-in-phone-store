53b2da067588724d7b95d935ceafd0c1a806fdd2
QWC\quinw <quinwacca@gmail.com>
2025-03-27 19:19:16 +0100

    add vitest config to load js files as jsx

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,6 +2,11 @@ import {defineConfig} from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
+  },
   plugins: [react()],
   resolve: {
     alias: {
