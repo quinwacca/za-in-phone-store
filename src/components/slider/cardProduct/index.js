ad953286a546c8215922c1bf877be9ce71b9cb0a
QWC\quinw <quinwacca@gmail.com>
2025-03-27 17:32:57 +0100

    add title to slider

--- src/components/slider/cardProduct/index.js
+++ src/components/slider/cardProduct/index.js
@@ -5,6 +5,7 @@ import styles from './index.module.css'
 const SliderCardProduct = ({products}) => {
   return (
     <div className={styles.slider}>
+      <h2 className={styles.listTitle}>Similar items</h2>
       <ul className={styles.list}>
         {products.map(({basePrice, brand, id, imageUrl, name}) => {
           return (
